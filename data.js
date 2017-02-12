b9a6021e6c26a368f80207a1f502bb3f2d62e646
Wil Asche <wil@asche.us>
2017-02-12 09:25:16 -0500

    add data and beginning of app

---
+++ data.js
@@ -0,0 +1,25 @@
+var data = {
+  achievements: {
+    loremaster: 11157,
+    azsuna: 10763,
+    valshara: 10698,
+    highmountain: 10059,
+    stormheim: 10790,
+    suramar: 11124,
+    insurrection: 11340
+  },
+  quests: {
+    arccos: 43318,
+    karazhan: 44556
+  },
+  professions: {
+    jc: 755
+  },
+  prof_quests: {
+    jc: [
+      40523, 40529, 40530, 40531, 40534, 40524, 40525, 42214, 40526, 40535,
+      40536, 40538, 40539, 40540, 40541, 40546, 40542, 40556, 40547, 40558,
+      40561, 40560, 40559, 40562
+    ]
+  }
+};
